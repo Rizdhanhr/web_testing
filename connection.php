e6db2873863b32fa355f455c3949f82e4f74d7f3
rizdhanhr <rizdhanhr@gmail.com>
2026-08-03 15:36:41 +0700

    tes vulnerabilty lg

--- connection.php
+++ connection.php
@@ -1,6 +1,17 @@
 <?php
 
-$abc = '113asdasddsa';
-$password = "1234567";
+// Contoh kode yang akan langsung terdeteksi oleh SonarQube
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "1234567"; // Hardcoded password yang langsung dipakai fungsi sensitif
+$db_name = "my_database";
 
+// Fungsi koneksi ini akan memicu rule S2068 di SonarQube
+$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$connection) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+echo "Koneksi berhasil!";
 
